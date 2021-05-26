28317ed595040e6b0b3cf65a84d99f2a2e6f1c53
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2021-05-26 10:03:24 -0500

    redeploy

---
+++ build/precache-manifest.64dc5926cd24018a07799103b46fdabb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0e320693f678136b984",
+    "url": "/Lights-Out/static/css/main.44ea94f1.chunk.css"
+  },
+  {
+    "revision": "b0e320693f678136b984",
+    "url": "/Lights-Out/static/js/main.ac39c0fc.chunk.js"
+  },
+  {
+    "revision": "e061cc9e69b74deb68ab",
+    "url": "/Lights-Out/static/js/runtime~main.daba1e25.js"
+  },
+  {
+    "revision": "ef82a0a30d88e0e0f1b7",
+    "url": "/Lights-Out/static/js/2.68448c7a.chunk.js"
+  },
+  {
+    "revision": "e2ac737088fc00f5a5e186d56d910e3f",
+    "url": "/Lights-Out/index.html"
+  }
+];
